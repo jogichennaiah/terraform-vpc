8b0f3976d955ba392103e4a159558b5468a137cb
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-17 23:46:24 +0530

    Auto push

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
 module "vpc" {
    source                    = "./vendor/modules/vpc"
-  VPC_CIDR                  = var.VPC_CIDR
-  ENV                       = var.ENV
-  AZ                        = var.AZ 
-  PUBLIC_SUBNET_CIDR        = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR       = var.PRIVATE_SUBNET_CIDR
-  DEFAULT_VPC_ID            = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR          = var.DEFAULT_VPC_CIDR
-  DEFAILT_VPC_RT            = var.DEFAILT_VPC_RT
-  PRIVATE_HOSTED_ZONE_ID    = var.PRIVATE_HOSTED_ZONE_ID
-  PRIVATE_HOSTED_ZONE_NAME  = var.PRIVATE_HOSTED_ZONE_NAME
-  PUBLIC_HOSTED_ZONE_ID     = var.PUBLIC_HOSTED_ZONE_ID
-  PUBLIC_HOSTED_ZONE_NAME   = var.PUBLIC_HOSTED_ZONE_NAME
-
+   ENV                       = var.ENV
 }

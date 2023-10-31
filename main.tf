3ae13cb1ec20d3f2cead427cde463ba177ed3b07
jogichennaiah <chennaiahjogi394@gmail.com>
2023-10-31 22:09:00 +0530

    Auto push

--- main.tf
+++ main.tf
@@ -3,8 +3,4 @@ module "vpc" {
   VPC_CIDR                  = var.VPC_CIDR
   ENV                       = var.ENV
   AZ                        = var.AZ 
-  PUBLIC_SUBNET_CIDR        = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR       = var.PRIVATE_SUBNET_CIDR
-  DEFAILT_VPC_RT            = var.DEFAILT_VPC_RT
-
 }
